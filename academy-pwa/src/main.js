1391a9b9ea1199127b7f4640da5c78584cef8dda
Iago Cavalcante <iagoangelimc@gmail.com>
2018-03-07 17:23:07 -0300

    Projetos inicial

---
+++ academy-pwa/src/main.js
@@ -0,0 +1,27 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify, {
+  primary: "#FF7043",
+  secondary: "#FF8A65",
+  accent: "#33691E",
+  error: "#f44336",
+  warning: "#ffeb3b",
+  info: "#2196f3",
+  success: "#4caf50"
+})
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
